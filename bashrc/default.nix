2140f8bf57b5a65e09738b494170d5730296cf2b
Nicolas Mattia <nicolas@nmattia.com>
2018-12-22 18:03:20 +0100

    Specify NIX_PATH from bashrc

--- bashrc/default.nix
+++ bashrc/default.nix
@@ -6,12 +6,15 @@
 #   if [ -x "$(command -v bashrc)" ]; then $(bashrc); fi
 { lib, writeText, writeScriptBin, fzf }:
 let
+  nixpkgsSpec =
+    (builtins.fromJSON (builtins.readFile ../nix/versions.json)).nixpkgs;
   bashrc = writeText "bashrc"
     (lib.concatStringsSep "\n"
     [ (builtins.readFile ./bashrc)
       ''
       source ${fzf}/share/fzf/completion.bash
       source ${fzf}/share/fzf/key-bindings.bash
+      NIX_PATH=nixpkgs=https://api.github.com/repos/${nixpkgsSpec.owner}/${nixpkgsSpec.repo}/tarball/${nixpkgsSpec.rev}
       ''
     ]
     );
