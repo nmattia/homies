4c8983bfaafe4fb6176aff0954350b206c13520c
Nicolas Mattia <nicolas@nmattia.com>
2024-09-22 13:43:20 +0200

    Add better zsh git completion

--- zshrc/default.nix
+++ zshrc/default.nix
@@ -3,12 +3,24 @@
 # Instead we read it from ~/.nix-profile.
 { lib, runCommand, writeText, writeScriptBin, fzf, nix, cacert, nixpkgs-src }:
 let
+
+  # official git completion, better & faster than the zsh defaults
+  git-completion-zsh-src = builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/git/git/3c20acdf465ba211978108ca8507d41e62a016fd/contrib/completion/git-completion.zsh";
+    sha256 = "sha256:0cifmyc0rsf1pn0lr4qpkgwcb2l7dxk8nqbd7xdc9ij3jq34ijnf";
+  };
+  git-completion-zsh = runCommand "git-completion-zsh" {} ''
+    mkdir -p $out/git-completion.zsh/
+    cat <${git-completion-zsh-src} > $out/git-completion.zsh/_git
+    '';
+
   # Write .zshrc to share/zshrc/zshrc
   zshrc = writeText "zshrc"
     (lib.concatStringsSep "\n"
       [ # Set up nix autocomplete
         ''
           fpath+=(${nix}/share/zsh/site-functions)
+          fpath+=(${git-completion-zsh}/git-completion.zsh)
         ''
         (builtins.readFile ./zshrc)
         # Set up fzf terminal bindings
